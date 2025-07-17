a902cc43a523497f71c9b2234e16f40b0c641597
YudaDh <yudadarmawan26@gmail.com>
2025-07-18 00:27:29 +0800

    modify cors

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ import morgan from "morgan";
 const app = express();
 
 const corsOptions = {
-   origin: 'http://localhost:5173', // Origin frontend Anda
+   origin: process.env.FRONTEND_URL, // Origin frontend Anda
    credentials: true, // Mengizinkan pengiriman cookie/credentials
 };
 
